9a8ab390829315abfc627add6dfd6d48db333655
Microsoft Tech Club LPS <45787667+MTCLPS@users.noreply.github.com>
2018-12-11 13:48:24 +0100

    Add files via upload

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    float speed = 10;
+
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.down * 8;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Racket")
+        {
+            float dis = (transform.position.x - col.transform.position.x) / col.collider.bounds.size.y;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-dis, 1).normalized * speed;
+        }
+        if (col.gameObject.tag == "Block")
+            Destroy(col.gameObject);
+    }
+}
---
+++ Assets/Script/Racket.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Racket : MonoBehaviour {
+
+    float speed = 15;
+
+	void Update () {
+        float v = Input.GetAxisRaw("Horizontal") * speed;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(v, 0);
+	}
+}
